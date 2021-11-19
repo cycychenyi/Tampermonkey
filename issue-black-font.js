6dbbd899d58843ba233c5276da11991606ff5483
cycychenyi <cycychenyi@outlook.com>
2021-11-19 18:08:32 +0800

    feat(issue-black-font): Trello GitHub issue 标签黑色字体

---
+++ issue-black-font.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Trello GitHub issue 标签黑色字体
+// @namespace    https://github.com/cycychenyi
+// @version      0.1
+// @description  Tello GitHub Power-Up issue 标签默认白色字体，我使用浅色标签居多，会导致看不清标签上的字，故设置为黑色字体。
+// @author       cycychenyi
+// @match        http*://github.trello.services/issues.html*
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @require      https://releases.jquery.com/git/jquery-git.min.js
+// @resource     waitForKeyElements https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// ==/UserScript==
+
+eval(GM_getResourceText('waitForKeyElements'));
+
+waitForKeyElements('.label', function() {
+    'use strict';
+    const labels = document.getElementsByClassName('label');
+    for (let i = 0; i < labels.length; ++i) {
+        labels[i].style.color = 'black';
+    }
+});
