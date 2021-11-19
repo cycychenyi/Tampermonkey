e9a9e768245806b2df681fb2bfed4dd6e2e29457
cycychenyi <cycychenyi@outlook.com>
2021-11-19 16:44:24 +0800

    fix(zoom-out-images): 修复题图失效的问题
    网站页面更新，没有 RSS 链接的图片了。

--- zoom-out-images.js
+++ zoom-out-images.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         阮一峰科技爱好者周刊图片缩小
 // @namespace    https://github.com/cycychenyi
-// @version      0.2
+// @version      0.3
 // @description  打开页面时，默认把图片都缩小到 20%，方便快速浏览。点击恢复原来大小，再点击缩小到 20%。
 // @author       cycychenyi
 // @match        http*://www.ruanyifeng.com/blog/*/weekly-issue-*.html
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
     const images = document.getElementsByTagName('img');
-    for (let i = 1; i < images.length; ++i) { // 第一张图片是 RSS 链接的图片，跳过
+    for (let i = 0; i < images.length; ++i) {
         images[i].style.maxWidth = '20%';
         images[i].onclick = function() {
             if (this.style.maxWidth === '') {
