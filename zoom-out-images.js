83ebfc61b7d5ce034762538eae96b30d5fb797b0
cycychenyi <cycychenyi@outlook.com>
2021-12-17 11:47:50 +0800

    feat(zoom-out-images): 优化体验，小图悬浮出现放大图标，大图悬浮出现缩小图标

--- zoom-out-images.js
+++ zoom-out-images.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         阮一峰科技爱好者周刊图片缩小
 // @namespace    https://greasyfork.org/zh-CN/users/459661-cycychenyi
-// @version      0.4
+// @version      0.5
 // @description  打开页面时，默认把图片都缩小到 20%，方便快速浏览。点击恢复原来大小，再点击缩小到 20%。
 // @author       cycychenyi
 // @match        http*://www.ruanyifeng.com/blog/*/weekly-issue-*.html
@@ -13,11 +13,14 @@
     const images = document.getElementsByTagName('img');
     for (let i = 0; i < images.length; ++i) {
         images[i].style.maxWidth = '20%';
+        images[i].style.cursor = 'zoom-in';
         images[i].onclick = function() {
             if (this.style.maxWidth === '') {
                 this.style.maxWidth = '20%';
+                images[i].style.cursor = 'zoom-in';
             } else {
                 this.style.maxWidth = '';
+                images[i].style.cursor = 'zoom-out';
             }
         }
     }
